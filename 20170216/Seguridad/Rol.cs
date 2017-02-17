6826495e040f912c3ebe6405e9c0817cf0b21924
fepc18 <fepc18@gmail.com>
2017-02-17 15:28:00 -0500

    Add files via upload
    seguridad

---
+++ 20170216/Seguridad/Rol.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modelo.Seguridad
+{
+    [Table("Roles")]
+    public class Rol
+    {
+        public int RolId { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String Nombre { get; set; }
+        public Aplicacion Aplicacion { get; set; }
+    }
+
+    public class UsuarioRol
+    {
+        public int UsuarioRolId { get; set; }
+        public string Usuario { get; set; }
+        public int RolId { get; set; }        
+    }
+    [Table("PermisosRol")]
+    public class PermisoRol
+    {
+        public int PermisosRolId { get; set; }
+        public int RolId { get; set; }
+        public ItemMenuAplicacion ItemMenuAplicacionId { get; set; }
+    }
+    [Table("ItemMenuAplicacion")]
+    public class ItemMenuAplicacion
+    {
+        public int ItemMenuAplicacionId { get; set; }
+        public string Opcion { get; set; }
+        public TipoItemMenu TipoItemMenuId { get; set; }        
+        public Aplicacion AplicacionId { get; set; }
+    }
+    public enum TipoItemMenu{
+        Item,
+        Menu
+    }
+    public enum Aplicacion{
+        ControlResidentes
+    }
+}
---
+++ 20170216/Seguridad/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modelo.Seguridad
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]        
+        public String Login { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String Identificacion { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String Nombres { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public String Apellidos { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public DateTime FechaIngreso { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public Boolean Activo { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public Boolean Bloqueado { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public DateTime? FechaUltimoCambioPassword { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        public int NumeroIntentosFallidos { get; set; }               
+
+    }
+}
